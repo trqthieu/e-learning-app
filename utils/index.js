167f4cc06246d05c4311453148dfbd936588ba05
trongtn <trong21082001@gmail.com>
2024-03-19 21:01:36 +0700

    feature/exercise: Update UI exercise

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,13 @@
 export function convertTime(minutes) {
-    let hours = Math.floor(minutes / 60);
-    let remainingMinutes = minutes % 60;
-    return hours + " hours and " + remainingMinutes + " minutes";
+  let hours = Math.floor(minutes / 60);
+  let remainingMinutes = minutes % 60;
+  return hours + " hours and " + remainingMinutes + " minutes";
 }
+
+export const sortDescending = (a, b) => {
+  return b.order - a.order;
+};
+
+export const sortAscending = (a, b) => {
+  return a.order - b.order;
+};
